25638aa2f43379fb323c2655f1ca8e629ed2a28b
Levi Gruspe <mail.levig@gmail.com>
2021-02-26 11:55:46 +0800

    Rewrite examples with deferred filters

--- examples/index.ts
+++ examples/index.ts
@@ -1,83 +1,31 @@
-import {
-  matches,
-  DomWriter,
-  Plugin,
-  Request,
-  Router
-} from '@lggruspe/fragment-router'
-
-class ColorChanger implements Plugin {
-  router: Router
-  constructor (router: Router) {
-    this.router = router
-  }
-
-  get req () {
-    return this.router.currentRequest()
-  }
-
-  enter () {}
-
-  exit () {
-    const color = this.req?.color
-    if (color) {
-      document.body.style.backgroundColor = color
-    }
-  }
-}
-
-class FontChanger implements Plugin {
-  router: Router
-  constructor (router: Router) {
-    this.router = router
-  }
-
-  get req () {
-    return this.router.currentRequest()
-  }
-
-  enter () {}
-
-  exit () {
-    const font = this.req?.font
-    if (font) {
-      document.body.style.fontFamily = font
-    }
-  }
-}
-
+import { check, matches, DomWriter, Router } from '@lggruspe/fragment-router'
 const router = new Router()
-const colorChanger = new ColorChanger(router)
 const writer = new DomWriter(router)
-const fontChanger = new FontChanger(router)
-router.stack.plugins.push(writer, colorChanger, fontChanger)
 
-function setResult (req: Request) {
-  req.result = '#' + req.id
-}
+router.route(
+  check(matches(/^color\/(?<color>[a-z]+)$/)),
+  req => {
+    const color = req.params?.color
+    if (color) {
+      router.defer(() => {
+        document.body.style.backgroundColor = color
+      })
+    }
+  },
+  req => {
+    router.defer(() => {
+      writer.renderHtml(`<p>#${req.id}</p>`)
+    })
+  }
+)
 
-router
-
-  .route(
-    req => {
-      if (!matches(/^color\/(?<color>[a-z]+)$/)(req)) {
-        throw req.control.next
-      }
-    },
-    req => {
-      const color = req.matched?.groups?.color
-      if (color) {
-        req.color = color
-      }
-    },
-    setResult
-  )
-
-  .route(
-    req => {
-      req.font = req.id
-    },
-    setResult
-  )
+router.route(
+  req => {
+    router.defer(() => {
+      document.body.style.fontFamily = req.id
+      writer.renderHtml(`<p>#${req.id}</p>`)
+    })
+  }
+)
 
 router.listen()
